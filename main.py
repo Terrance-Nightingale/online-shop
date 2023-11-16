c4ab40765381d788153e9b201824ffba6bb35aa8
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-11-16 10:31:59 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask import Flask, jsonify, render_template, request
+from flask_bootstrap import Bootstrap4
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
+
